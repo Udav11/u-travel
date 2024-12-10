af28bbe7221e0ef2864d28b83cbba1ef7e02e710
Udav <udavimpinf@gmail.com>
2024-12-10 20:34:03 +0300

    Start

---
+++ main.py
@@ -0,0 +1,13 @@
+
+
+
+
+
+
+check_button = False
+print('Перемести чара в локу Mawar Gorge и не двигайся')
+while not check_button:
+    start_button = input('Нажми Y для старта \n')
+    if start_button == 'y' or start_button == "Y":
+        check_button = True
+        
